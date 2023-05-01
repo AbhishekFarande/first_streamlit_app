10771b2e5a9a179216aab8ad17c1e7a3c56b9922
AbhishekFarande <AbhishekFarande@users.noreply.github.com>
2023-05-01 13:10:05 -0400

    Update streamlit_app.py
    snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display formatted string in tabular formatted
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
